c8c8afe1e6f77e26a34f6b1e7becf85ba9b8036a
Andrew Safwat <andrewsafwatsamuel@gmail.com>
2023-06-16 12:02:20 +0300

    created employee registration

--- registration/main.js
+++ registration/main.js
@@ -50,9 +50,9 @@ document.getElementById("company-registration-form").addEventListener("submit", 
   formData.append('password', passwordInput.value);
 
   const registerUrl = 'http://localhost:5000/companies/register';
-const redirectOnSuccess = '../published_jobs/published_jobs.html';
+  const redirectOnSuccess = '../published_jobs/published_jobs.html';
 
-  register(formData, registerUrl, redirectOnSuccess,'company')
+  register(formData, registerUrl, redirectOnSuccess, 'company')
 
   // Retrieve form data and perform further actions (e.g., AJAX request)
   console.log("Form submitted!");
@@ -62,10 +62,26 @@ const redirectOnSuccess = '../published_jobs/published_jobs.html';
 
 document.getElementById("employee-registration-form").addEventListener("submit", function (e) {
   e.preventDefault();
-  var form = e.target;
-  var formData = new FormData(form);
+  const name = document.getElementById('employee-name');
+  const bio = document.getElementById('employee-bio');
+  const phone = document.getElementById('employee-phone');
+  const email = document.getElementById('employee-email');
+  const title = document.getElementById('employee-title');
+  const password = document.getElementById('employee-password');
 
+  var formData = new FormData();
 
+  formData.append('name', name.value)
+  formData.append('bio', bio.value)
+  formData.append('phone', phone.value)
+  formData.append('email', email.value)
+  formData.append('title', title.value)
+  formData.append('password', password.value)
+
+  const registerUrl = 'http://localhost:5000/employees/register';
+  const redirectOnSuccess = '../search-job/search-job.html';
+
+  register(formData, registerUrl, redirectOnSuccess, 'employee')
 
   console.log("Form submitted!");
   console.log("Form Data:", Object.fromEntries(formData.entries()));
@@ -90,6 +106,8 @@ async function register(form_data, register_endpoint, redirect_on_success, user_
     sessionStorage.setItem("session_data", JSON.stringify(session_data));
     window.location.replace(redirect_on_success);
   } else {
-    console.log(`Error: ${response.status}`);
+    const session_data = await response.text();
+
+    console.log(`Error: ${session_data}`);
   }
 }

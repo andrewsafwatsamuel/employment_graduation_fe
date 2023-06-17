0ddc1037e6b9a23de75f70caa5bfad767d5b0a44
Andrew Safwat <andrewsafwatsamuel@gmail.com>
2023-06-18 01:38:10 +0300

    dded edit job endpoint

--- job_info/job_info.js
+++ job_info/job_info.js
@@ -5,7 +5,7 @@ const is_company = session_data['user_type'].toLowerCase() == 'company'
 const urlParams = new URLSearchParams(window.location.search);
 const jsonString = urlParams.get("job");
 console.log(jsonString)
-const job = JSON.parse(jsonString);
+var job = JSON.parse(jsonString);
 
 // job info
 const company_name_container = document.getElementById('company_name_container');
@@ -14,6 +14,18 @@ const job_title = document.getElementById('jobTitle');
 const job_description = document.getElementById('jobDescription');
 const job_exp_level = document.getElementById('experienceLevel');
 const job_status = document.getElementById('jobStatus');
+
+// edit job modal
+const editJobModal = document.getElementById('editJobModal');
+const editJobTitleInput = document.getElementById('editJobTitle');
+const editJobDescriptionInput = document.getElementById('editJobDescription');
+const editExperienceLevelSelect = document.querySelector('editExperienceLevel')
+const saveJobDetailsBtn = document.getElementById('saveJobDetailsBtn');
+const jobTitleText = document.getElementById('jobTitle');
+const jobDescriptionText = document.getElementById('jobDescription');
+const experienceLevelText = document.getElementById('experienceLevel');
+const status_input = document.getElementById('editJobStatus');
+
 
 // employee views
 const btn_apply = document.getElementById('applyBtn');
@@ -45,10 +57,14 @@ if (is_employee) {
 }
 
 
+document.addEventListener('DOMContentLoaded', function () {
+    M.AutoInit();
+});
+
 // update hob status
 btn_toggle_job_status.addEventListener('click', () => {
-    toggle_update_job_status(job.id, () => {
-        job['status'] = job['status'] == 1 ? 0 : 1
+    toggle_update_job_status(job.id, (json) => {
+        job['status'] = json.status
         job_status.textContent = job.status == 1 ? 'Open' : 'Closed'
     })
 })
@@ -65,9 +81,9 @@ async function toggle_update_job_status(job_id, on_success) {
             body: formData
         });
     if (response.ok) {
-        on_success()
         const update_job_status_response = await response.json(); // get the json of the jobs from the response endpoint
         console.log(JSON.stringify(update_job_status_response))
+        on_success(update_job_status_response)
     } else {
         const error_response = await response.text();
         console.log(`Error: ${error_response}`);
@@ -75,63 +91,51 @@ async function toggle_update_job_status(job_id, on_success) {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    M.AutoInit();
-
-    // document.getElementById('companyName').innerText = 'Apple';
-    // document.getElementById('jobTitle').innerText = 'Software Engineer';
-    // document.getElementById('jobDescription').innerText = generateJobDescription();
-    // document.getElementById('experienceLevel').innerText = 'Entry Level';
-
-    const editExperienceLevelSelect = document.getElementById('editExperienceLevel');
-    const jobStatusSelect = document.getElementById('jobStatusSelect');
-
-    M.FormSelect.init(editExperienceLevelSelect);
-    M.FormSelect.init(jobStatusSelect);
+btn_edit_job_details.addEventListener('click', () => {
+    editJobModal.open();
 });
 
-function generateJobDescription() {
-    return 'This is a random job description.';
+saveJobDetailsBtn.addEventListener('click', () => {
+    console.log(status_input.value.toLowerCase() == "open" ? 1 : 0);
+    update_job_info(
+        document.getElementById('editExperienceLevel').value,
+        editJobDescriptionInput.value,
+        editJobTitleInput.value,
+        job.id,
+        (status_input.value.toLowerCase() == "open" ? 1 : 0),
+        (json) => {
+            job = json
+            company_name.textContent = is_employee ? job.company : ""
+            job_title.textContent = job.title
+            job_description.textContent = job.description
+            job_exp_level.textContent = job.exp_level
+            job_status.textContent = job.status == 1 ? 'Open' : 'Closed'
+            editJobModal.close();
+        }
+    );
+
+});
+
+async function update_job_info(exp_level, discription, title, job_id, status, on_success) {
+    const formData = new FormData();
+    formData.append('exp_level', exp_level);
+    formData.append('description', discription);
+    formData.append('title', title)
+    formData.append('status', status)
+    const response = await fetch('http://localhost:5000/job-listing/' + job_id,
+        {
+            method: 'PUT',
+            headers: {
+                'Authorization': session_data['auth_token']
+            },
+            body: formData
+        });
+    if (response.ok) {
+        const update_job_info_response = await response.json(); // get the json of the jobs from the response endpoint
+        console.log(JSON.stringify(update_job_info_response))
+        on_success(update_job_info_response)
+    } else {
+        const error_response = await response.text();
+        console.log(`Error: ${error_response}`);
+    }
 }
-
-document.getElementById('applyBtn').addEventListener('click', function () {
-    document.getElementById('applicationStatus').innerText = 'Application submitted';
-});
-
-const editJobModal = document.getElementById('editJobModal');
-const editJobTitleInput = document.getElementById('editJobTitle');
-const editJobDescriptionInput = document.getElementById('editJobDescription');
-const editExperienceLevelSelect = document.querySelector('#editExperienceLevel')
-const saveJobDetailsBtn = document.getElementById('saveJobDetailsBtn');
-const jobTitleText = document.getElementById('jobTitle');
-const jobDescriptionText = document.getElementById('jobDescription');
-const experienceLevelText = document.getElementById('experienceLevel');
-
-document.getElementById('editJobModal').addEventListener('click', function () {
-    editJobTitleInput.value = jobTitleText.innerText;
-    editJobDescriptionInput.value = jobDescriptionText.innerText;
-    M.FormSelect.init(editExperienceLevelSelect);
-    M.updateTextFields();
-});
-
-saveJobDetailsBtn.addEventListener('click', function () {
-    jobTitleText.innerText = editJobTitleInput.value;
-    jobDescriptionText.innerText = editJobDescriptionInput.value;
-    experienceLevelText.innerText = editExperienceLevelSelect.options[editExperienceLevelSelect.selectedIndex].text;
-    M.Modal.getInstance(editJobModal).close();
-});

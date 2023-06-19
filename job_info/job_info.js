ddaf7bd9e2ded0f891d1e6b35de88cd252e1c5b2
Andrew Safwat <andrewsafwatsamuel@gmail.com>
2023-06-19 03:48:59 +0300

    applied job details functionality

--- job_info/job_info.js
+++ job_info/job_info.js
@@ -47,7 +47,6 @@ job_status.textContent = job.status == 1 ? 'Open' : 'Closed'
 
 if (is_company) {
     company_name_container.remove();
-    applications_status_container.remove()
     btn_apply.style.display = 'none';
 }
 
@@ -107,7 +106,7 @@ saveJobDetailsBtn.addEventListener('click', () => {
         job.id,
         (status_input.value.toLowerCase() == "open" ? 1 : 0),
         (json) => {
-            if(json['message']==null)job = json
+            if (json['message'] == null) job = json
             company_name.textContent = is_employee ? job.company : ""
             job_title.textContent = job.title
             job_description.textContent = job.description
@@ -141,4 +140,55 @@ async function update_job_info(exp_level, discription, title, job_id, status, on
         const error_response = await response.text();
         console.log(`Error: ${error_response}`);
     }
-}
+};
+
+btn_view_applications.addEventListener('click', () => {
+    view_job_applications((json)=>{
+        
+    })
+});
+
+async function view_job_applications(on_success) {
+    const response = await fetch('http://localhost:5000/job-listing/' + job.id + '/applications',
+        {
+            method: 'GET',
+            headers: {
+                'Authorization': session_data['auth_token']
+            }
+        });
+    
+    if (response.ok) {
+        const get_job_applications = await response.json(); // get the json of the jobs from the response endpoint
+        console.log(JSON.stringify(get_job_applications))
+        on_success(get_job_applications)
+    } else {
+        const error_response = await response.text();
+        console.log(`Error: ${error_response}`);
+    }
+};
+
+
+btn_apply.addEventListener('click',()=>{
+    apply_to_job()
+});
+
+async function apply_to_job() {
+    const form = new FormData();
+    form.append('job_listing_id',job['id'])
+    const response = await fetch('http://localhost:5000/job-listing/apply',
+        {
+            method: 'POST',
+            headers: {
+                'Authorization': session_data['auth_token']
+            },
+            body : form
+        });
+    if (response.ok) {
+        const apply_to_job_response = await response.json(); // get the json of the jobs from the response endpoint
+        console.log(JSON.stringify(apply_to_job_response))
+    } else {
+        const error_response = await response.text();
+        console.log(`Error: ${error_response}`);
+    }
+};
+
